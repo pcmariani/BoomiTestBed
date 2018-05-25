fd1f8128203b3893a29c543d19124dea0130fdde
Blake Rhodes <blakewrhodes@gmail.com>
2018-05-25 17:23:27 -0500

    handle file exceptions, increased sass

--- src/ScriptRunner.groovy
+++ src/ScriptRunner.groovy
@@ -1,20 +1,29 @@
-
 class ScriptRunner {
 
-    String run(String scriptName, String dataDocument){
-        DataContext dataContext = new DataContext(new FileInputStream(dataDocument), new Properties())
+    String run(String scriptName, String dataDocument) {
+        FileInputStream fileInputStream
+        try {
+            fileInputStream = new FileInputStream(dataDocument)
+        } catch (Exception e) {
+            return "I can't find the document ${dataDocument}"
+        }
 
-        String script = new File(scriptName).text
+        DataContext dataContext = new DataContext(fileInputStream, new Properties())
+        String script
+        try {
+            script = new File(scriptName).text
+        } catch (Exception e) {
+            return "I can't find the script ${scriptName}"
+        }
         try {
             Eval.x(dataContext, "def dataContext = x; ${script} ; return dataContext")
-        }
-        catch(Exception e){
-            return "That script sucked: ${e.message}"
+        } catch (Exception e) {
+            return "That script does not make sense to me:\n ${e.message}"
         }
         "Resulting Document\n" +
-        "=============================\n" +
-        "${dataContext.is.text}\n" +
-        "=============================\n" +
-        "Resulting Props${dataContext.props}"
+                "=============================\n" +
+                "${dataContext.is.text}\n" +
+                "=============================\n" +
+                "Resulting Props${dataContext.props}"
     }
 }
