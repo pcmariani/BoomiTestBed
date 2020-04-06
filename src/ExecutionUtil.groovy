f43a445afc43b395449a44ec66db2645971044ae
Pete Mariani <pcmariani@gmail.com>
2020-04-06 20:43:19 +0100

    initial commit

---
+++ src/ExecutionUtil.groovy
@@ -0,0 +1,18 @@
+import java.util.logging.Logger;
+
+public class ExecutionUtil {
+    static def dynamicProcessProperties = new Properties();
+
+    static void setDynamicProcessProperty(String key, String value, boolean persist) {
+        dynamicProcessProperties.setProperty(key, value)
+    }
+
+    static def getDynamicProcessProperty(String key) {
+        return dynamicProcessProperties.getProperty(key)
+    }
+
+    static Logger getBaseLogger() {
+        System.setProperty("java.util.logging.SimpleFormatter.format", "%5\$s %n")
+        return Logger.getAnonymousLogger()
+    }
+}
