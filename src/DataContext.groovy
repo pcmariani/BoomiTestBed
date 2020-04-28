3f192bb433534221a8ba57b60bc50a0a753cd8cd
Pete Mariani <pcmariani@gmail.com>
2020-04-28 15:23:18 +0100

    added pretty printing

--- src/DataContext.groovy
+++ src/DataContext.groovy
@@ -102,29 +102,3 @@ class DataContext {
 
     }
 }
-
-
-// class DataContext {
-//     InputStream is
-//     Properties props
-
-//     DataContext(InputStream inputStream, Properties properties){
-//         is = inputStream
-//         props = properties
-//     }
-
-//     void storeStream(InputStream inputStream, Properties properties){
-//         is = inputStream
-//         props = properties
-//     }
-
-//     int getDataCount(){
-//         1
-//     }
-//     InputStream getStream(int index){
-//         is
-//     }
-//     Properties getProperties(int index) {
-//         props
-//     }
-// }
