e4c6c4815d539a62f10a9d5099b26ac741145fd4
Pete Mariani <pcmariani@gmail.com>
2020-04-28 14:20:58 +0100

    added BoomiTestBed.groovy; replaces GroovyTestBed.groovy

---
+++ src/BoomiTestBed.groovy
@@ -0,0 +1,29 @@
+class BoomiTestBed {
+    static void main(String[] args) throws Exception {
+        def cli = new CliBuilder(usage: 'BoomiTestBed.groovy [-h] [-d document] [-p properties] script')
+
+        cli.with {
+            h longOpt: 'help', 'Show usage information (you\'re here ;)'
+            d longOpt: 'document', args: 1, argName: 'documentName', 'Sets the source for incoming document'
+            p longOpt: 'properties', args: 1, argName: 'propertiesName', 'Sets the source properties'
+        }
+
+        def options = cli.parse(args)
+
+        if (options.h) {
+            cli.usage()
+            return
+        }
+
+        String dataDocumentName = options.d ? options.d : null
+        String propertiesFileName = options.p ? options.p : null
+        String scriptName = options.arguments()[0]
+
+        if (scriptName == null) {
+            cli.usage()
+            return
+        }
+
+        println new ScriptRunner(new FileService(), new EvalService()).run(scriptName, dataDocumentName, propertiesFileName)
+    }
+}
