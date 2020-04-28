e4992834634093bdb5e03804e31438c545353770
Pete Mariani <pcmariani@gmail.com>
2020-04-28 15:24:20 +0100

    added dynamic process props

--- src/EvalService.groovy
+++ src/EvalService.groovy
@@ -5,10 +5,3 @@ class EvalService implements MockableService {
         "${script}; return dataContext")
     }
 }
-
-
-// class EvalService implements MockableService {
-//     void eval(DataContext dataContext, String script) {
-//         Eval.x(dataContext, "def dataContext = x; ${script} ; return dataContext")
-//     }
-// }
