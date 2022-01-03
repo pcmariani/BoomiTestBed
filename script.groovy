aff55157cca17cfe2ac3cb33feb70e1bc81de879
Pete Mariani <pcmariani@gmail.com>
2022-01-03 14:19:03 -0500

    changed commandline options

--- script.groovy
+++
@@ -1,8 +0,0 @@
-for ( int i = 0; i < dataContext.getDataCount(); i++ ) {
-    InputStream is = dataContext.getStream(i)
-    //noinspection GroovyAssignabilityCheck
-    Properties props = dataContext.getProperties(i) as Properties
-    props.setProperty("key", "value")
-    String result = "${is.text}\nGood bye World!"
-    dataContext.storeStream(new ByteArrayInputStream(result.getBytes("UTF-8")), props)
-}
--- src/BoomiTestBed.groovy
+++ src/BoomiTestBed.groovy
@@ -1,11 +1,16 @@
+import boomitestbed.*
+
 class BoomiTestBed {
     static void main(String[] args) throws Exception {
         def cli = new CliBuilder(usage: 'BoomiTestBed.groovy [-h] [-d document] [-p properties] script')
 
         cli.with {
-            h longOpt: 'help', 'Show usage information (you\'re here ;)'
-            d longOpt: 'document', args: 1, argName: 'documentName', 'Sets the source for incoming document'
-            p longOpt: 'properties', args: 1, argName: 'propertiesName', 'Sets the source properties'
+            h  longOpt: 'help', 'Show usage information (you\'re here ;)'
+            d  longOpt: 'document', args: 1, argName: 'documentName', 'Sets the source for incoming document'
+            p  longOpt: 'properties', args: 1, argName: 'propertiesName', 'Sets the source properties'
+            // xr longOpt: 'no result', 'don\t output result'
+            // xp longOpt: 'no DPPs', 'don\t output dynamic process properties'
+            // xd longOpt: 'no DDPs', 'don\t output dynamic document properties'
         }
 
         def options = cli.parse(args)
@@ -15,9 +20,9 @@ class BoomiTestBed {
             return
         }
 
+        String scriptName = options.arguments()[0]
         String dataDocumentName = options.d ? options.d : null
         String propertiesFileName = options.p ? options.p : null
-        String scriptName = options.arguments()[0]
 
         if (scriptName == null) {
             cli.usage()
