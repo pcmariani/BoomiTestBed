079c4acc42f22604c7a93b43865824ea24760853
Pete Mariani <pcmariani@gmail.com>
2022-01-03 16:02:59 -0500

    cleanup

--- src/boomitestbed/EvalService.groovy
+++ src/boomitestbed/EvalService.groovy
@@ -3,9 +3,6 @@ package boomitestbed
 class EvalService implements MockableService {
     void eval(DataContext dataContext, ExecutionUtilHelper ExecutionUtil, String script) {
         Eval.xy(dataContext, ExecutionUtil, "def dataContext = x; def ExecutionUtil = y;" +
-        "println 'p2';" +
-        // "ExecutionUtil eu = ExecutionUtil.instance;" +
-        // "ExecutionUtil.dynamicProcessProperties = dynamicProcessProperties;" +
         "${script}; return dataContext")
     }
 }
