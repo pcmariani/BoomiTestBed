edcf8f20ef7602970bf599086fce223279b89367
Pete Mariani <pcmariani@gmail.com>
2022-10-19 15:51:13 -0400

    now handles -p and -d args

--- BoomiTestBed.sh
+++ BoomiTestBed.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 
+newArgs=""
+while getopts "d:p:" arg; do
+    case $arg in
+        d)  newArgs+="-d $(pwd)/"${OPTARG}" ";;
+        # d)  documentFilesArr=(${OPTARG// / })
+        #     for i in "${!documentFilesArr[@]}"; do
+        #       documentFilesArr[i]="$(pwd)/${documentFilesArr[i]}"
+        #     done
+        #     newArgs+="-d '${documentFilesArr[@]}' "
+        #     ;;
+        p)  newArgs+="-p $(pwd)/"${OPTARG}" ";;
+    esac
+done
+newArgs+="$(pwd)/${@: -1}"
+# echo "newArgs: $newArgs"
+
+pushd $BOOMI_HOME > /dev/null
+groovyclient "$BOOMI_HOME"/BoomiTestBed.groovy $newArgs
+popd > /dev/null
+
 # if !"$BOOMI_HOME"; then
-  # echo "ERROR: Need to set BOOMI_HOME"
+# echo "ERROR: Need to set BOOMI_HOME"
 # fi
 
-# echo $(pwd)/$1
-dir=$(pwd)
-pushd $BOOMI_HOME > /dev/null
-groovyclient "$BOOMI_HOME"/BoomiTestBed.groovy $dir/$*
-popd > /dev/null
