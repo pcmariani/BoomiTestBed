3e4a6d6cea578e2563fa0694abe18c65654e0fae
Peter Mariani <pcmariani@gmail.com>
2023-01-06 10:31:46 -0500

    updated BoomiTestTed.sh with debug messages

--- BoomiTestBed.sh
+++ BoomiTestBed.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DEBUG=1
+
 newArgs=""
 while getopts "d:p:" arg; do
     case $arg in
@@ -14,11 +16,27 @@ while getopts "d:p:" arg; do
     esac
 done
 newArgs+="$(pwd)/${@: -1}"
-# echo "newArgs: $newArgs"
+
+[ "$DEBUG" -eq 1 ] && echo "BoomiTestBed.sh DEBUG..."
+[ "$DEBUG" -eq 1 ] && echo "> BOOMI_HOME: $BOOMI_HOME"
+[ "$DEBUG" -eq 1 ] && echo "> pwd: $(pwd)"
+[ "$DEBUG" -eq 1 ] && echo "> pushd"
 
 pushd $BOOMI_HOME > /dev/null
+
+[ "$DEBUG" -eq 1 ] && echo "> pwd: $(pwd)"
+[ "$DEBUG" -eq 1 ] && echo "> newArgs: $newArgs"
+[ "$DEBUG" -eq 1 ] && echo "> groovyclient $BOOMI_HOME/BoomiTestBed.groovy $newArgs"
+[ "$DEBUG" -eq 1 ] && echo ""
+
 groovyclient "$BOOMI_HOME"/BoomiTestBed.groovy $newArgs
+
+[ "$DEBUG" -eq 1 ] && echo "> popd"
+
 popd > /dev/null
+
+[ "$DEBUG" -eq 1 ] && echo "> pwd: $(pwd)"
+[ "$DEBUG" -eq 1 ] && echo ""
 
 # if !"$BOOMI_HOME"; then
 # echo "ERROR: Need to set BOOMI_HOME"
