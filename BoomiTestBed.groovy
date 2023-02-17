adbb6bf7f176f96651e2634e04f2051cdd76fe1b
Pete Mariani <pcmariani@gmail.com>
2023-02-16 20:56:12 -0700

    added -f

--- BoomiTestBed.groovy
+++ BoomiTestBed.groovy
@@ -2,18 +2,19 @@
 
 class BoomiTestBed {
     static void main(String[] args) throws Exception {
-        def cli = new CliBuilder(usage: 'BoomiTestBed.groovy [-hn] -s script [-d document] [-p properties] [-e extension] [-w working-dir]')
+        def cli = new CliBuilder(usage: 'BoomiTestBed.groovy [-h][-n][-f][-xd][-xp] -s script [-d document] [-p properties] [-e extension] [-w working-dir] [-rp pattern]')
 
         cli.with {
-            h  longOpt: 'help', 'Show usage (you\'re here ;)'
-            s  longOpt: 'script', args: 1, argName: 'scriptName', 'Sets the source for the script'
-            d  longOpt: 'document', args: 1, argName: 'documentName', 'Sets the source for incoming document'
-            p  longOpt: 'properties', args: 1, argName: 'propertiesName', 'Sets the source properties'
-            e  longOpt: 'extension', args: 1, argName: 'outFileExtension', 'Extension of output file'
-            w  longOpt: 'working-dir', args: 1, argName: 'workingDir', 'Present Working Directory'
+            h  longOpt: 'help', 'Show usage'
+            s  longOpt: 'script', args: 1, argName: 'fileName', 'Name of the script'
+            d  longOpt: 'document', args: 1, argName: 'fileName', 'Name of input document'
+            p  longOpt: 'properties', args: 1, argName: 'fileName', 'Name of input properties'
+            f  longOpt: 'output-file', type: boolean, 'Output to file'
+            e  longOpt: 'extension', args: 1, argName: 'fileExtension', 'Extension of output file'
+            w  longOpt: 'working-dir', args: 1, argName: 'dir', 'Present Working Directory'
             xd longOpt: 'suppress-data-output', type: boolean, 'Suppresses output of data'
             xp longOpt: 'suppress-props-output', type: boolean, 'Suppresses output of props'
-            rp longOpt: 'ddp-replace-pattern', args: 1, argName: 'ddpPreplacePattern', 'Props to prevent from appearing'
+            rp longOpt: 'ddp-replace-pattern', args: 1, argName: 'regex', 'Prevent props from appearing'
         }
 
         def options = cli.parse(args)
@@ -28,14 +29,10 @@ class BoomiTestBed {
         String dataDocumentName = options.d ? workingDir + "/" + options.d : null
         String propertiesFileName = options.p ? workingDir + "/" + options.p : null
         String outFileExtension = options.e ? options.e : "dat"
+        Boolean outToFile = options.f
         Boolean suppressData = options.xd
         Boolean suppressProps = options.xp
         String ddpPreplacePattern = options.rp ? options.rp : null
-
-        if (scriptName == null) {
-            cli.usage()
-            return
-        }
 
         // println "PWD: " + System.getProperty("user.dir")
         // println "workingDir: " + options.w
@@ -43,8 +40,14 @@ class BoomiTestBed {
         // println "dataDocumentName: " + dataDocumentName
         // println "propertiesFileName: " + propertiesFileName
         // println "outFileExtension: " + outFileExtension
+        // println "outToFile: " + outToFile
         // println "Suppress Output: " + suppressResult
 
-        println new ScriptRunner().run(scriptName, dataDocumentName, propertiesFileName, outFileExtension, suppressData, suppressProps, ddpPreplacePattern)
+        if (scriptName == null) {
+            cli.usage()
+            return
+        }
+
+        println new ScriptRunner2().run(scriptName, dataDocumentName, propertiesFileName, outFileExtension, suppressData, suppressProps, ddpPreplacePattern, outToFile)
     }
 }
