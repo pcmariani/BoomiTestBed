53a2ae81ca48df41ee2b346e30db82e507788bcf
Pete Mariani <pcmariani@gmail.com>
2023-07-12 17:30:15 -0400

    added exitCode handling

--- boomi.sh
+++ boomi.sh
@@ -4,4 +4,10 @@ workingDir="$(pwd)"
 
 pushd $BOOMI_HOME > /dev/null
 groovy "$BOOMI_HOME"/BoomiTestBed.groovy $@ -w "$workingDir"
+exitCode="$?"
+
 popd > /dev/null
+
+if [[ "$exitCode" != "0" ]]; then
+    exit 1
+fi
